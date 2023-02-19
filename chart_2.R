8ea8028ea8a53980d3536e727861c3d2ac4dfc05
gillianmeis <gillms4@uw.edu>
2023-02-18 16:43:53 -0800

    created chart regarding philanthropy score and privilege or self-made status

--- chart_2.R
+++ chart_2.R
@@ -1,1 +1,18 @@
-# test
+#Load Library
+library("tidyverse")
+library("dplyr")
+library("ggplot2")
+library("RColorBrewer")
+
+# Load dataset
+data <- na.omit(read.csv("final_dataframe.csv"))
+
+# Creating plot
+data %>% ggplot(aes(x = philanthropyScore, fill=selfMade)) +
+  geom_bar(position="stack") + 
+  labs(title = "Philanthropy Score vs. Number of Billionaires",
+       x = "Philanthropy Score (1-5)",
+       y = "Number of Billionaires",
+       fill = "SelfMade") +
+  scale_color_manual(values=c("#ADBDFF", "#3185FC")) 
+
