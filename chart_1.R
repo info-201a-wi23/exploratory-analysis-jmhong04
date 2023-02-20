8a948547878b199d070fba151c8a1207e75a17fb
ayeshapatel03 <ayeshap@uw.edu>
2023-02-20 13:33:49 -0800

    fix legend labels

--- chart_1.R
+++ chart_1.R
@@ -9,6 +9,8 @@ chart_world_data <- all_data %>%
   select(age, finalWorth, gender) %>%
   filter(gender == 'F' | gender == 'M')
 
+chart_world_data <- chart_world_data %>%
+  
 na.omit(chart_world_data)
 
 ggplot(data = chart_world_data) +
@@ -16,4 +18,5 @@ ggplot(data = chart_world_data) +
   geom_point(mapping = aes(x = age, y = finalWorth, color = gender)) +
   scale_x_continuous(breaks = seq(20, 100, 5)) + 
   scale_y_continuous(breaks = seq(0, 220, 25)) + 
-  labs(title = "Distribution of Age vs Net Worth by Gender", x = "Age", y = "Net Worth (in billions)", fill = "Gender")
+  labs(title = "Distribution of Age vs Net Worth by Gender", x = "Age", y = "Net Worth (in billions)", color = "Gender") +
+  scale_color_hue(labels = c('Women', 'Men'))
