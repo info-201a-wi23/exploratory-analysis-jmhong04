3be6420b944c4702b6bb2de7c3843284eecff90a
megancuan <mcuan@uw.edu>
2023-02-20 11:45:17 -0800

    Minnor edits

--- chart_3.R
+++ chart_3.R
@@ -32,12 +32,12 @@ billionaire_shape_state_data <- state_shape %>%
   left_join(billionaire_amt_state, by = c("updated_regions" = "state"))
 
 ggplot(data = billionaire_shape_state_data) +
-  geom_polygon(aes(x = Longitude, 
-                   y = Latitude, 
+  geom_polygon(aes(x = long, 
+                   y = lat, 
                    group = group,
                    fill = state_number)) +
   coord_map() +
-  labs(title = 'Billionaires in the US', fill = 'Number of Billionaires')
+  labs(title = 'Billionaires in the US', fill = 'Number of Billionaires', x = 'Longitude', y = 'Latitude')
 
 #World Map
 my_countries <- world_shape$region
@@ -49,11 +49,11 @@ billionaire_shape_world_data <- world_shape %>%
 
 
 world_map <- ggplot(data = billionaire_shape_world_data) +
-  geom_polygon(aes(x = Longitude, 
-                   y = Latitude, 
+  geom_polygon(aes(x = long, 
+                   y = lat, 
                    group = group,
                    fill = country_number)) +
   coord_map() +
-  labs(title = 'Billionaires Around the World', fill = 'Number of Billionaires')
+  labs(title = 'Billionaires Around the World', fill = 'Number of Billionaires', x = 'Longitude', y = 'Latitude')
 
 world_map + coord_map(xlim=c(-180,180))
