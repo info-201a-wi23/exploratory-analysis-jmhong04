2bd784b06434565161d0273ab855c8bb5ffeb4e3
ayeshapatel03 <ayeshap@uw.edu>
2023-02-19 16:19:33 -0800

    clean up code and export chart

--- chart_1.R
+++ chart_1.R
@@ -12,9 +12,8 @@ chart_world_data <- all_data %>%
 na.omit(chart_world_data)
 
 ggplot(data = chart_world_data) +
-  geom_point(mapping = aes(x = age, y = finalWorth, color = gender)) + 
+  geom_line(mapping = aes(x = age, y = finalWorth, color = gender)) +
+  geom_point(mapping = aes(x = age, y = finalWorth, color = gender)) +
   scale_x_continuous(breaks = seq(20, 100, 5)) + 
   scale_y_continuous(breaks = seq(0, 220, 25)) + 
   labs(title = "Distribution of Age vs Net Worth by Gender", x = "Age", y = "Net Worth (in billions)", fill = "Gender")
-
-?labs()
