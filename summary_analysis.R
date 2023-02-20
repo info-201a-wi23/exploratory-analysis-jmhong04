c4c32ac41e4218b405ab2e352f36e9b0f1e2e57e
fali2003 <fali2003@uw.edu>
2023-02-20 03:28:40 -0800

    wrote first draft of summary statistics

---
+++ summary_analysis.R
@@ -0,0 +1,63 @@
+billionaire_data <- read.csv("C:\\Users\\iyoab\\Desktop\\INFO201\\exploratory-analysis-jmhong04\\final_dataframe.csv", stringsAsFactors = FALSE)
+library("dplyr")
+library("tidyverse")
+library("stringr")
+
+# AGE: WHAT IS THE MEDIAN AND MEAN AGE?
+average_age <- billionaires_data %>% 
+  summarize(mean_age = mean(age, na.rm = TRUE))
+median_age <- billionaires_data %>% 
+  summarize(median_age = median(age, na.rm = TRUE))
+
+# GENDER
+total_num_billionaires <- nrow(billionaires_data)
+
+gender_variable <- billionaires_data$gender
+num_of_men <- length(gender_variable[gender_variable == 'M'])
+num_of_women <- length(gender_variable[gender_variable == "F"])
+perc_of_women <- num_of_women/total_num_billionaires
+perc_of_men <- num_of_men/total_num_billionaires
+
+
+# PRIVILEGE
+amt_inherited <- length(privilege[privilege == "False"])
+amt_selfMade <- length(privilege[privilege == "True"])
+
+proportion_inherited <- amt_inherited/total_num_billionaires
+proportion_selfMade <- amt_selfMade/total_num_billionaires
+
+##LOCATION
+country_amt_billionaires <- billionaire_data %>% 
+  group_by(country) %>% 
+  summarize(total_billionaires = n())
+
+highest_num_country <- country_amt_billionaires %>% 
+  filter(total_billionaires == max(total_billionaires, na.rm = TRUE)) %>% 
+  pull(country)
+
+state_amt_billionaires <- billionaire_data %>%
+  filter(country == "United States") %>% 
+  group_by(state) %>% 
+  summarize(num_billionaires_state = n())
+
+highest_num_state <- state_amt_billionaires %>% 
+  filter(num_billionaires_state == max(num_billionaires_state, na.rm = TRUE)) %>% 
+  pull(state)
+
+#PHILANTROPHY
+avg_philantrophy <- billionaires_data %>% 
+  filter(country == "United States") %>% 
+  summarize(average_philantrophy_score = mean(philanthropyScore, na.rm = TRUE))
+
+#Summary Statistics
+summary_info <- list()
+summary_info$meanAge <- average_age
+summary_info$median_Age <- median_age_bill
+summary_info$proportion_of_women <- percentage_of_women
+summary_info$proportion_of_men <- percentage_of_men
+summary_info$proportion_inherited <- prop_privilege
+summary_info$proportion_selfmade <- prop_selfMade
+summary_info$most_country <- highest_num_billionaires_country
+summary_info$most_state <- highest_num_billionaires_state
+summary_info$mean_philantrophy <- avg_philantrophy_score
+summary_info$total_amt <- total_amt_billionaires
