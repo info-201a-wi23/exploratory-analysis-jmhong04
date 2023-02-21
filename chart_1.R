dcc1d877992e81574cbfce31dff118bfe58b5687
jmhong04 <jmhong04@uw.edu>
2023-02-20 19:56:01 -0800

    use styler on chart1 code

--- chart_1.R
+++ chart_1.R
@@ -7,16 +7,15 @@ all_data <- read.csv("final_dataframe.csv", stringsAsFactors = FALSE)
 
 chart_world_data <- all_data %>%
   select(age, finalWorth, gender) %>%
-  filter(gender == 'F' | gender == 'M')
+  filter(gender == "F" | gender == "M")
 
 chart_world_data <- chart_world_data %>%
-  
-na.omit(chart_world_data)
+  na.omit(chart_world_data)
 
 ggplot(data = chart_world_data) +
   geom_line(mapping = aes(x = age, y = finalWorth, color = gender)) +
   geom_point(mapping = aes(x = age, y = finalWorth, color = gender)) +
-  scale_x_continuous(breaks = seq(20, 100, 5)) + 
-  scale_y_continuous(breaks = seq(0, 220, 25)) + 
+  scale_x_continuous(breaks = seq(20, 100, 5)) +
+  scale_y_continuous(breaks = seq(0, 220, 25)) +
   labs(title = "Distribution of Age vs Net Worth by Gender", x = "Age", y = "Net Worth (in billions)", color = "Gender") +
-  scale_color_hue(labels = c('Women', 'Men'))
+  scale_color_hue(labels = c("Women", "Men"))
