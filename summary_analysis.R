c620b94365c13822735248d129ba8663d9a7c6e4
fali2003 <fali2003@uw.edu>
2023-02-20 18:02:25 -0800

    re-run file

--- summary_analysis.R
+++ summary_analysis.R
@@ -1,4 +1,4 @@
-# billionaire_data <- read.csv("C:\\Users\\iyoab\\Desktop\\INFO201\\exploratory-analysis-jmhong04\\final_dataframe.csv", stringsAsFactors = FALSE)
+billionaire_data <- read.csv("C:\\Users\\iyoab\\Desktop\\INFO201\\exploratory-analysis-jmhong04\\final_dataframe.csv", stringsAsFactors = FALSE)
 library("dplyr")
 library("tidyverse")
 library("stringr")
@@ -11,7 +11,7 @@ average_age <- billionaire_data %>%
 total_num_billionaires <- nrow(billionaire_data)
 
 gender_variable <- billionaire_data$gender
-num_of_men <- length(gender_variable[gender_variable == 'M'])
+num_of_men <- length(gender_variable[gender_variable == "M"])
 num_of_women <- length(gender_variable[gender_variable == "F"])
 perc_of_women <- num_of_women/total_num_billionaires
 perc_of_men <- num_of_men/total_num_billionaires
@@ -50,11 +50,11 @@ avg_philantrophy <- billionaire_data %>%
 
 #Summary Statistics
 summary_info <- list()
-summary_info$averageAge <- average_age
-summary_info$proportion_of_women <- perc_of_women
-summary_info$proportion_of_men <- perc_of_men
+summary_info$average_age <- average_age
+summary_info$perc_of_women <- perc_of_women
+summary_info$perc_of_men <- perc_of_men
 summary_info$proportion_inherited <- proportion_inherited
-summary_info$proportion_selfmade <- proportion_selfMade
-summary_info$most_country <- highest_num_country
-summary_info$most_state <- highest_num_state
-summary_info$mean_philantrophy <- avg_philantrophy
+summary_info$proportion_selfMade <- proportion_selfMade
+summary_info$highest_num_country <- highest_num_country
+summary_info$highest_num_state <- highest_num_state
+summary_info$avg_philantrophy <- avg_philantrophy
