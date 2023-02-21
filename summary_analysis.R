f3a781fa588df4b878f7ac33090ece583b64538c
fali2003 <fali2003@uw.edu>
2023-02-20 20:10:57 -0800

    styled the file

--- summary_analysis.R
+++ summary_analysis.R
@@ -1,11 +1,12 @@
-#billionaire_data <- read.csv("C:\\Users\\iyoab\\Desktop\\INFO201\\exploratory-analysis-jmhong04\\final_dataframe.csv", stringsAsFactors = FALSE)
+# billionaire_data <- read.csv("C:\\Users\\iyoab\\Desktop\\INFO201\\exploratory-analysis-jmhong04\\final_dataframe.csv", stringsAsFactors = FALSE)
 library("dplyr")
 library("tidyverse")
 library("stringr")
+library("styler")
 
 # AGE: WHAT IS THE MEDIAN AND MEAN AGE?
-average_age <- billionaire_data %>% 
-  summarize(median_age = median(age, na.rm = TRUE)) %>% 
+average_age <- billionaire_data %>%
+  summarize(median_age = median(age, na.rm = TRUE)) %>%
   pull(median_age)
 
 # GENDER
@@ -14,8 +15,8 @@ total_num_billionaires <- nrow(billionaire_data)
 gender_variable <- billionaire_data$gender
 num_of_men <- length(gender_variable[gender_variable == "M"])
 num_of_women <- length(gender_variable[gender_variable == "F"])
-perc_of_women <- num_of_women/total_num_billionaires
-perc_of_men <- num_of_men/total_num_billionaires
+perc_of_women <- num_of_women / total_num_billionaires
+perc_of_men <- num_of_men / total_num_billionaires
 
 
 # PRIVILEGE
@@ -23,34 +24,34 @@ Privilege <- billionaire_data$selfMade
 amt_inherited <- length(Privilege[Privilege == "False"])
 amt_selfMade <- length(Privilege[Privilege == "True"])
 
-proportion_inherited <- amt_inherited/total_num_billionaires
-proportion_selfMade <- amt_selfMade/total_num_billionaires
+proportion_inherited <- amt_inherited / total_num_billionaires
+proportion_selfMade <- amt_selfMade / total_num_billionaires
 
-##LOCATION
-country_amt_billionaires <- billionaire_data %>% 
-  group_by(country) %>% 
+## LOCATION
+country_amt_billionaires <- billionaire_data %>%
+  group_by(country) %>%
   summarize(total_billionaires = n())
 
-highest_num_country <- country_amt_billionaires %>% 
-  filter(total_billionaires == max(total_billionaires, na.rm = TRUE)) %>% 
+highest_num_country <- country_amt_billionaires %>%
+  filter(total_billionaires == max(total_billionaires, na.rm = TRUE)) %>%
   pull(country)
 
 state_amt_billionaires <- billionaire_data %>%
-  filter(country == "United States") %>% 
-  group_by(state) %>% 
+  filter(country == "United States") %>%
+  group_by(state) %>%
   summarize(num_billionaires_state = n())
 
-highest_num_state <- state_amt_billionaires %>% 
-  filter(num_billionaires_state == max(num_billionaires_state, na.rm = TRUE)) %>% 
+highest_num_state <- state_amt_billionaires %>%
+  filter(num_billionaires_state == max(num_billionaires_state, na.rm = TRUE)) %>%
   pull(state)
 
-#PHILANTROPHY
-avg_philantrophy <- billionaire_data %>% 
-  filter(country == "United States") %>% 
-  summarize(average_philantrophy_score = mean(philanthropyScore, na.rm = TRUE)) %>% 
+# PHILANTROPHY
+avg_philantrophy <- billionaire_data %>%
+  filter(country == "United States") %>%
+  summarize(average_philantrophy_score = mean(philanthropyScore, na.rm = TRUE)) %>%
   pull(average_philantrophy_score)
 
-#Summary Statistics
+# Summary Statistics
 summary_info <- list()
 summary_info$average_age <- average_age
 summary_info$perc_of_women <- perc_of_women
