0477520a2dad0dffcaf6040773e1c34d2dd2d7ab
jmhong04 <jmhong04@uw.edu>
2023-02-20 19:57:29 -0800

    use styler on chart 2 code

--- chart_2.R
+++ chart_2.R
@@ -1,4 +1,4 @@
-#Load Library
+# Load Library
 library("tidyverse")
 library("dplyr")
 library("ggplot2")
@@ -8,12 +8,13 @@ library("RColorBrewer")
 data <- na.omit(read.csv("final_dataframe.csv"))
 
 # Creating plot
-data %>% ggplot(aes(x = philanthropyScore, fill=selfMade)) +
-  geom_bar(position="stack", width = 0.75) + 
-  labs(title = "Philanthropy Score vs. Number of Billionaires",
-       x = "Philanthropy Score (1-5)",
-       y = "Number of Billionaires",
-       fill = "Source of Wealth", ) +
-  scale_fill_discrete(labels=c('Not Self-Made', 'Self-Made'), guide = guide_legend(reverse=TRUE)) +
-  scale_color_manual(values=c("#ADBDFF", "#3185FC")) 
-
+data %>% ggplot(aes(x = philanthropyScore, fill = selfMade)) +
+  geom_bar(position = "stack", width = 0.75) +
+  labs(
+    title = "Philanthropy Score vs. Number of Billionaires",
+    x = "Philanthropy Score (1-5)",
+    y = "Number of Billionaires",
+    fill = "Source of Wealth",
+  ) +
+  scale_fill_discrete(labels = c("Not Self-Made", "Self-Made"), guide = guide_legend(reverse = TRUE)) +
+  scale_color_manual(values = c("#ADBDFF", "#3185FC"))
