9d1074a659ae45bc10a0d14d08d6ca37d40b62f7
jmhong04 <jmhong04@uw.edu>
2023-02-20 19:45:06 -0800

    use stylr on chart3 code

--- chart_3.R
+++ chart_3.R
@@ -10,54 +10,62 @@ library("stringr")
 
 billionaire_data <- read.csv("final_dataframe.csv", stringsAsFactors = FALSE)
 
-billionaire_amt_state <- billionaire_data %>% 
-  filter(country == "United States") %>% 
+billionaire_amt_state <- billionaire_data %>%
+  filter(country == "United States") %>%
   group_by(state) %>%
   summarize(state_number = n())
 
-billionaire_amt_country <- billionaire_data %>% 
+billionaire_amt_country <- billionaire_data %>%
   group_by(country) %>%
   summarize(country_number = n())
 
 state_shape <- map_data("state")
 world_shape <- map_data("world")
 
-#State Map
+# State Map
 my_regions <- state_shape$region
 updated_regions <- str_to_title(my_regions)
-state_shape <- state_shape %>% 
+state_shape <- state_shape %>%
   mutate(updated_regions)
 
-billionaire_shape_state_data <- state_shape %>% 
+billionaire_shape_state_data <- state_shape %>%
   left_join(billionaire_amt_state, by = c("updated_regions" = "state"))
 
 ggplot(data = billionaire_shape_state_data) +
-  geom_polygon(aes(x = long, 
-                   y = lat, 
-                   group = group,
-                   fill = state_number)) +
+  geom_polygon(aes(
+    x = long,
+    y = lat,
+    group = group,
+    fill = state_number
+  )) +
   coord_map() +
-  scale_fill_gradient(low="#A2A2D0", 
-                      high="#132B43") +
-  labs(title = 'Billionaires in the US', fill = 'Number of Billionaires', x = 'Longitude', y = 'Latitude')
+  scale_fill_gradient(
+    low = "#A2A2D0",
+    high = "#132B43"
+  ) +
+  labs(title = "Billionaires in the US", fill = "Number of Billionaires", x = "Longitude", y = "Latitude")
 
-#World Map
+# World Map
 my_countries <- world_shape$region
 updated_countries <- str_replace(my_countries, "USA", "United States")
-world_shape <- world_shape %>% 
+world_shape <- world_shape %>%
   mutate(updated_countries)
-billionaire_shape_world_data <- world_shape %>% 
+billionaire_shape_world_data <- world_shape %>%
   left_join(billionaire_amt_country, by = c("updated_countries" = "country"))
 
 
 world_map <- ggplot(data = billionaire_shape_world_data) +
-  geom_polygon(aes(x = long, 
-                   y = lat, 
-                   group = group,
-                   fill = country_number)) +
+  geom_polygon(aes(
+    x = long,
+    y = lat,
+    group = group,
+    fill = country_number
+  )) +
   coord_map() +
-  scale_fill_gradient(low="#A2A2D0", 
-                      high="#132B43") +
-  labs(title = 'Billionaires Around the World', fill = 'Number of Billionaires', x = 'Longitude', y = 'Latitude')
+  scale_fill_gradient(
+    low = "#A2A2D0",
+    high = "#132B43"
+  ) +
+  labs(title = "Billionaires Around the World", fill = "Number of Billionaires", x = "Longitude", y = "Latitude")
 
-world_map + coord_map(xlim=c(-180,180))
+world_map + coord_map(xlim = c(-180, 180))
